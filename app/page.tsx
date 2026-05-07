4635455a0fbda513fe35b45b5ed28c2073f90c91
AlexMcHugh1 <alexanderedwardmchugh@gmail.com>
2026-05-07 23:21:29 +0100

    feat: reorder sections, remove About
    Order: Experience -> Certifications -> Education -> GitHub ->
    Open source -> Writing. About section deleted, hero nav and section
    numbers updated to match.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Hero from '@/components/Hero';
-import About from '@/components/About';
 import Experience from '@/components/Experience';
-import Contributions from '@/components/Contributions';
-import GitHub from '@/components/GitHub';
-import Education from '@/components/Education';
 import Certifications from '@/components/Certifications';
+import Education from '@/components/Education';
+import GitHub from '@/components/GitHub';
+import Contributions from '@/components/Contributions';
 import Articles from '@/components/Articles';
 import Footer from '@/components/Footer';
 import RevealOnScroll from '@/components/RevealOnScroll';
@@ -14,12 +13,11 @@ export default function Home() {
     <>
       <main className="relative">
         <Hero />
-        <About />
         <Experience />
-        <Contributions />
-        <GitHub />
-        <Education />
         <Certifications />
+        <Education />
+        <GitHub />
+        <Contributions />
         <Articles />
       </main>
       <Footer />
